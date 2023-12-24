70a837ca0a432d1d44ff330c84fdc61d25c4485e
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-12-24 12:22:56 +0200

    added javascript tot it

---
+++ scripts/header.js
@@ -0,0 +1,21 @@
+const myLinks = document.querySelectorAll("div.links-container > a");
+function checkUnderline(element)
+{
+    const boolValue = element.classList.contains("underline");
+    if (boolValue)
+    {
+        element.classList.remove("underline");
+    }
+}
+function checkListener(event)
+{
+    event.preventDefault(); 
+    myLinks.forEach(checkUnderline);
+    const elementClassList = event.currentTarget.classList;
+    elementClassList.add("underline");
+}
+function listenEvent(element)
+{
+    element.addEventListener("click", checkListener);
+}
+myLinks.forEach(listenEvent);
